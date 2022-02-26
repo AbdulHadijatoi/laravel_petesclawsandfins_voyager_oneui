2cf876e6c583cf2bc8c7fcfda5cf3e83b16c6343
H∆DI <hadi@thiqatech.com>
2022-02-26 07:23:07 +0500

    recreating the whole project using Voyager and oneui

---
+++ resources/views/vendor/voyager/pages/edit-add.blade.php
@@ -0,0 +1,12 @@
+@php
+    $edit = !is_null($dataTypeContent->getKey());
+    $add  = is_null($dataTypeContent->getKey());
+@endphp
+
+@extends('voyager::master')
+
+@section('css')
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+@stop
+
+@section('page_title', __('voyager::generic.'.($edit ? 'edit' : 'add')).' '.$dataType->getTranslatedAttribute('display_name_singular'))
